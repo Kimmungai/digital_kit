2bd6984781956db70d3a1bde9748ca22fb69381e
kimmungai <kimpita9@gmail.com>
2018-04-06 22:30:36 +0900

    kusonga

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -34,23 +34,29 @@
           <aside ng-cloak>
             <div class="panel">
                <ul class="shelf-icons">
-                  <li ng-class="active_shelf==1 ? 'active' : 'inactive'" ng-click="active_shelf=1;shelf_items=1"><a href="#"><i class="fa fa-globe"></i></a></li>
-                  <li ng-class="active_shelf==2 ? 'active' : 'inactive'" ng-click="active_shelf=2;shelf_items=2"><a href="#"><i class="fa fa-bullseye"></i></a></li>
-                  <li ng-class="active_shelf==3 ? 'active' : 'inactive'" ng-click="active_shelf=3;shelf_items=3"><a href="#"><i class="fa fa-question"></i></a></li>
+                  <li ng-class="active_shelf==1 ? 'active' : 'inactive'" ng-click="active_shelf=1;active_tool=1" ng-show="tool_A==true"><a href="#"><i class="fa fa-globe"></i></a></li>
+                  <li ng-class="active_shelf==2 ? 'active' : 'inactive'" ng-click="active_shelf=2"><a href="#"><i class="fa fa-bullseye"></i></a></li>
+                  <li ng-class="active_shelf==3 ? 'active' : 'inactive'" ng-click="active_shelf=3;active_tool=2" ng-show="tool_B==true"><a href="#"><i class="fa fa-book"></i></a></li>
+                  <li ng-class="active_shelf==4 ? 'active' : 'inactive'" ng-click="active_shelf=4;active_tool=3" ng-show="tool_C==true"><a href="#"><i class="fa fa-file"></i></a></li>
+                  <li ng-class="active_shelf==5 ? 'active' : 'inactive'" ng-click="active_shelf=5;active_tool=4" ng-show="tool_D==true"><a href="#"><i class="fa fa-envelope"></i></a></li>
+                  <li ng-class="active_shelf==6 ? 'active' : 'inactive'" ng-click="active_shelf=6"><a href="#"><i class="fa fa-question"></i></a></li>
                </ul>
                 <ul class="shelves">
-                  <li ng-click="active_shelf=1;shelf_items=1"><a href="#">Website</a></li>
-                  <li ng-click="active_shelf=2;shelf_items=1">Theme: <select><option>Light</option><option>Dark</option></select></li>
-                  <li ng-click="active_shelf=3;shelf_items=1"><a href="#"> Help</a></li>
+                  <li ng-click="active_shelf=1;active_tool=1" ng-show="tool_A==true"><a href="#">Website</a></li>
+                  <li ng-click="active_shelf=2">Theme: <select><option>Light</option><option>Dark</option></select></li>
+                  <li ng-click="active_shelf=3;active_tool=2" ng-show="tool_B==true"><a href="#">Business Card</a></li>
+                  <li ng-click="active_shelf=4;active_tool=3" ng-show="tool_C==true"><a href="#">Curriculum Vitae</a></li>
+                  <li ng-click="active_shelf=5;active_tool=4" ng-show="tool_D==true"><a href="#">Letter</a></li>
+                  <li ng-click="active_shelf=6"><a href="#"> Help</a></li>
                 </ul>
             </div>
             <div class="panel">
               <section class="tools">
                 <ul>
-                  <li><input type="checkbox"  ng-model="tool_A"  /> Website</li>
-                  <li><input type="checkbox" ng-model="tool_B"/> Card</li>
-                  <li><input type="checkbox" ng-model="tool_C"/> Cv</li>
-                  <li><input type="checkbox" ng-model="tool_D"/> Letter</li>
+                  <li><input type="checkbox"  ng-model="tool_A"  ng-click="active_shelf=1;active_tool=1" /> Website</li>
+                  <li><input type="checkbox" ng-model="tool_B" ng-click="active_shelf=3;active_tool=2" /> Card</li>
+                  <li><input type="checkbox" ng-model="tool_C" ng-click="active_shelf=4;active_tool=3"/> Cv</li>
+                  <li><input type="checkbox" ng-model="tool_D" ng-click="active_shelf=5;active_tool=4"/> Letter</li>
                 </ul>
               </section>
             </div>
@@ -59,18 +65,18 @@
               <section class="desk" ng-show="my_tools==1">
               <h2><i class="fa fa-briefcase" aria-hidden="true"></i> My tools <span class="close" ng-click="my_tools=0;"><i class="fa fa-close" aria-hidden="true"></i></span></h2>
               <ul class="mb3">
-                <li ng-show="tool_A==1" ng-class="active_tool==1 ? 'active' : 'inactive'" ng-click="active_tool=1" class="A">
+                <li ng-show="tool_A==1" ng-class="active_tool==1 ? 'active' : 'inactive'" ng-click="active_shelf=1;active_tool=1" class="A">
                   <a href="#"><i class="fa fa-globe" aria-hidden="true"></i></a>
                 </li>
-                <li ng-show="tool_B==1"  ng-class="active_tool==2 ? 'active' : 'inactive'" ng-click="active_tool=2" class="B">
+                <li ng-show="tool_B==1"  ng-class="active_tool==2 ? 'active' : 'inactive'" ng-click="active_shelf=3;active_tool=2" class="B">
                   <a href="#"><i class="fa fa-address-card" aria-hidden="true"></i></a>
                 </li>
               </ul>
               <ul class="mb3">
-                <li ng-show="tool_C==1"  ng-class="active_tool==3 ? 'active' : 'inactive'" ng-click="active_tool=3" class="C">
+                <li ng-show="tool_C==1"  ng-class="active_tool==3 ? 'active' : 'inactive'" ng-click="active_shelf=4;active_tool=3" class="C">
                   <a href="#"><i class="fa fa-file" aria-hidden="true"></i></a>
                 </li>
-                <li ng-show="tool_D==1"  ng-class="active_tool==4 ? 'active' : 'inactive'" ng-click="active_tool=4" class="D">
+                <li ng-show="tool_D==1"  ng-class="active_tool==4 ? 'active' : 'inactive'" ng-click="active_shelf=5;active_tool=4" class="D">
                   <a href="#"><i class="fa fa-list-alt" aria-hidden="true"></i></a>
                 </li>
               </ul>
